d264898872e9c12b4c6bfcf5b83bdbad2e61a297
ODonnellBBC <alex.odonnell@bbc.co.uk>
2022-12-14 15:22:24 +0000

    updated card styling

--- bbc-social/src/components/CardGroup.js
+++ bbc-social/src/components/CardGroup.js
@@ -1,13 +1,22 @@
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
 
+const cardStyle = { 
+  background: '#222222',
+  color: '#eeeeee',
+};
+
+cardStyle['font-family'] = 'ReithSans, Arial, Helvetica, freesans, sans-serif';
+cardStyle['border-radius'] = 0;
+cardStyle['--bs-card-border-width'] = '5px';
+
 function GroupExample() {
   return (
     <CardGroup>
-      <Card>
+      <Card style={cardStyle}>
         <Card.Img variant="top" src="holder.js/100px160" />
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
+          <Card.Title><b>Card title</b></Card.Title>
           <Card.Text>
             This is a wider card with supporting text below as a natural lead-in
             to additional content. This content is a little bit longer.
@@ -17,10 +26,10 @@ function GroupExample() {
           <small className="text-muted">Last updated 3 mins ago</small>
         </Card.Footer>
       </Card>
-      <Card>
+      <Card style={cardStyle}>
         <Card.Img variant="top" src="holder.js/100px160" />
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
+          <Card.Title><b>Card title</b></Card.Title>
           <Card.Text>
             This card has supporting text below as a natural lead-in to
             additional content.{" "}
@@ -30,10 +39,10 @@ function GroupExample() {
           <small className="text-muted">Last updated 3 mins ago</small>
         </Card.Footer>
       </Card>
-      <Card>
+      <Card style={cardStyle}>
         <Card.Img variant="top" src="holder.js/100px160" />
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
+          <Card.Title><b>Card title</b></Card.Title>
           <Card.Text>
             This is a wider card with supporting text below as a natural lead-in
             to additional content. This card has even longer content than the
